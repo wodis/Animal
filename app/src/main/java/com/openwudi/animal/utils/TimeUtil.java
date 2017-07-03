146b72d1c92bb211872fb0a5f5a22d4dd8ae716a
wudi <wodis1988@gmail.com>
2017-07-03 18:45:49 +0800

    去掉ws依赖

---
+++ app/src/main/java/com/openwudi/animal/utils/TimeUtil.java
@@ -0,0 +1,15 @@
+package com.openwudi.animal.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by diwu on 17/7/3.
+ */
+
+public class TimeUtil {
+    public static String getDateTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS");
+        return sdf.format(new Date());
+    }
+}
