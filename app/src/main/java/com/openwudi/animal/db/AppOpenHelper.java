b7c96e6ca8b0cb161a3489d03cbc0be65377c1ec
wudi <wodis1988@gmail.com>
2017-10-12 18:23:33 +0800

    fix bug

--- app/src/main/java/com/openwudi/animal/db/AppOpenHelper.java
+++ app/src/main/java/com/openwudi/animal/db/AppOpenHelper.java
@@ -3,6 +3,10 @@ package com.openwudi.animal.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.blankj.utilcode.utils.SPUtils;
+import com.openwudi.animal.base.AnimalApplication;
+import com.openwudi.animal.utils.Constants;
+
 import org.greenrobot.greendao.database.Database;
 
 /**
@@ -23,5 +27,7 @@ public class AppOpenHelper extends DaoMaster.OpenHelper {
         // TODO: 16/12/30 do nothing
         DaoMaster.dropAllTables(db, true);
         DaoMaster.createAllTables(db, false);
+        SPUtils spUtils = new SPUtils(AnimalApplication.INSTANCE, Constants.SP_CONFIG);
+        spUtils.clear();
     }
 }
